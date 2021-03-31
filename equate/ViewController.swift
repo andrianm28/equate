33ab2e9ae3145fc39ab83a033ae7af5a16b99fd6
andrianm28 <andrianmaulana28@gmail.com>
2021-03-31 16:59:06 +0700

    Initial Commit

---
+++ equate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  equate
+//
+//  Created by M. Andrian Maulana on 31/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
