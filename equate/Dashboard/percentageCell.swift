5902dfd2c6f634bb96f0a31ae75b12030f30d226
Devin Winardi <devinwinardi@Devins-MacBook-Pro.local>
2021-04-07 19:39:43 +0700

    dashboard page created

---
+++ equate/Dashboard/percentageCell.swift
@@ -0,0 +1,16 @@
+//
+//  percentageCell.swift
+//  equate
+//
+//  Created by Devin Winardi on 06/04/21.
+//
+
+import UIKit
+
+class percentageCell: UICollectionViewCell {
+    
+    @IBOutlet weak var cellTitle: UILabel!
+    @IBOutlet weak var cellPercent: UILabel!
+    @IBOutlet weak var cellInfo: UILabel!
+    
+}
