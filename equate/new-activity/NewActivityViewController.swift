625bb1f242a5232acef20bf30b713cdb3f3379a7
alifmahardhika <alif.mahardhika@ui.ac.id>
2021-04-09 15:36:44 +0700

    finale

--- equate/new-activity/NewActivityViewController.swift
+++ equate/new-activity/NewActivityViewController.swift
@@ -6,14 +6,19 @@
 //
 
 import UIKit
+protocol isAbleToReceiveData {
+  func pass(data: String)  //data: string is an example parameter
+}
 
-class NewActivityViewController: UIViewController {
-
+class NewActivityViewController: UIViewController, isAbleToReceiveData {
+    
     @IBOutlet weak var goalNameButton: UIButton!
     @IBOutlet weak var categoryButton: UIButton!
     @IBOutlet weak var iconButton: UIButton!
     @IBOutlet weak var repSwitch: UISwitch!
     @IBOutlet var dayCollection: [UIButton]!
+    @IBOutlet weak var acivityNameValue: UILabel!
+    var newActivity = Activity(name: "")
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,7 +28,16 @@ class NewActivityViewController: UIViewController {
         for i in chevronArr{
             setButton(butt: i!)
         }
+        acivityNameValue.text = self.newActivity.name
     }
+    
+    func pass(data: String) { //conforms to protocol
+        print("called")
+        print(data)
+        newActivity.name=data
+        acivityNameValue.text = data
+      // implement your own implementation
+       }
     @IBAction func selectDayRepeat(_ sender: UIButton) {
         if repSwitch.isOn{repSwitch.isOn = !repSwitch.isOn}
         print(String(sender.titleLabel!.text!))
@@ -49,6 +63,7 @@ class NewActivityViewController: UIViewController {
 //        present(inputGoalController(), animated: true)
         let vc = storyboard?.instantiateViewController(identifier: "inputGoal") as! inputGoalController
 //      present new storyboard
+        vc.delegate = self
         present(vc, animated: true)
     }
 //    dismiss storyboard
@@ -61,20 +76,26 @@ class NewActivityViewController: UIViewController {
 //    INPUT GOAL NAME CONTROLLER
 }
 class inputGoalController: UIViewController {
-
+    var delegate: isAbleToReceiveData!
+    @IBOutlet weak var nameTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    func viewWillDisappear() {
+          delegate.pass(data: "someData") //call the func in the previous vc
+      }
     @IBAction func closethisshit(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
     @IBAction func doneInput(_ sender: Any) {
 //        TODO SAVE INPUT
         dismiss(animated: true, completion: nil)
+        delegate.pass(data: nameTextField.text!)
 
     }
 }
 
-struct activity {
+struct Activity {
     var name: String!
 }
