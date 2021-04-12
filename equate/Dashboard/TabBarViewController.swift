f9340db95967a024717cb165196059dbb0836bd5
devin1727 <devinwinardi1727@gmail.com>
2021-04-12 11:43:06 +0700

    perbaiki button di dashboard dan add tab bar icon

---
+++ equate/Dashboard/TabBarViewController.swift
@@ -0,0 +1,18 @@
+//
+//  TabBarViewController.swift
+//  equate
+//
+//  Created by Devin Winardi on 11/04/21.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController {
+
+    @IBOutlet weak var myTabBar: UITabBar!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
