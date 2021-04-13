000011e0699dd971efb38fabf40758eb7fbf2ec2
alifmahardhika <alif.mahardhika@ui.ac.id>
2021-04-13 14:32:47 +0700

    resolve timepicker issue

--- equate/new-activity/NewActivityViewController.swift
+++ equate/new-activity/NewActivityViewController.swift
@@ -102,7 +102,10 @@ class NewActivityViewController: UIViewController, isAbleToReceiveData {
         timeFormatter.timeStyle = DateFormatter.Style.short
         timeFormatter.timeZone = TimeZone(abbreviation: "GMT+7:00")
 //        TODO JAMNYA SALAH
-        newGoal.time = timePicker.date//timeFormatter.date(from: timeFormatter.string(from: timePicker.date))
+        let calendar = Calendar.current
+        let date = calendar.date(byAdding: .hour, value: 7, to: timePicker.date)
+        print(date!)
+        newGoal.time = date//timeFormatter.date(from: timeFormatter.string(from: timePicker.date))
         
         let components = Calendar.current.dateComponents([.hour, .minute], from: activityDurationValue.date)
         let hour = components.hour!
